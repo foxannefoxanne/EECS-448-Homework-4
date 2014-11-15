c742c2faa0aec72535e3da5038b43ea4d3a65e2f
foxannefoxanne <rox.j.calderon@gmail.com>
2014-11-15 17:44:45 -0600

    Getting some classes set up for view
    :dancers: For some reason Github is letting me add emojis. Just wanted
    to have the classes for everyone to link to. They might not all end up
    being ints, but a few of them are returning ints.

--- src/View.java
+++ src/View.java
@@ -3,54 +3,73 @@ import java.awt.BorderLayout;
 
 public class View {
 
-	private JFrame frame; 
-	private JLabel label;
-	private JButton button; 
-	ButtonGroup starter = null;
-
-	public View(String text)
-	{
-		frame = new JFrame("View"); 
-		frame.getContentPane().setLayout(new BorderLayout()); 
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		frame.setSize(200,200);
-		frame.setVisible(true);
-		 
-		label = new JLabel(text); 
-		frame.getContentPane().add(label, BorderLayout.CENTER);
-	      starter = new ButtonGroup();
-	      JPanel p = new JPanel();
-	   
-	      addOption(p,starter,"View Books ");
-	      addOption(p,starter,"Order Books");
-	      addOption(p,starter,"Remove Books");
-	      addOption(p,starter,"View Order" );
-	      frame.add(p);
-
-		button = new JButton("Select"); 
-		frame.getContentPane().add(button, BorderLayout.SOUTH);
-		
+//	private JFrame frame; 
+//	private JLabel label;
+//	private JButton button; 
+//	ButtonGroup starter = null;
+//
+//	public View(String text)
+//	{
+//		frame = new JFrame("View"); 
+//		frame.getContentPane().setLayout(new BorderLayout()); 
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+//		frame.setSize(200,200);
+//		frame.setVisible(true);
+//		 
+//		label = new JLabel(text); 
+//		frame.getContentPane().add(label, BorderLayout.CENTER);
+//	      starter = new ButtonGroup();
+//	      JPanel p = new JPanel();
+//	   
+//	      addOption(p,starter,"View Books ");
+//	      addOption(p,starter,"Order Books");
+//	      addOption(p,starter,"Remove Books");
+//	      addOption(p,starter,"View Order" );
+//	      frame.add(p);
+//
+//		button = new JButton("Select"); 
+//		frame.getContentPane().add(button, BorderLayout.SOUTH);
+//		
+//	}
+//	
+//	public JButton getButton()
+//	{
+//		return button; 
+//	}
+//	
+//	public void setText(String text)
+//	{
+//		label.setText(text); 
+//	}
+//	
+//	 public void addOption(JPanel p, ButtonGroup g, String t) {
+//	      JRadioButton b = new JRadioButton(t);
+//	      b.setActionCommand(t);
+//	      p.add(b);
+//	      g.add(b);
+//	   }
+	  int displayMenu()
+	  {
+		  return 0; 
+	  }
+	  
+	  int displayCategories()
+	  {
+		  return 0; 
+	  }
+	  
+	  int displayBooks()
+	  {
+		  return 0;
+	  }
+	  
+	  int cart()
+	  {
+		  return 45; 
+	  }
+	  
+	  int RemoveOption()
+	  {
+		  return 333; 
+	  }
 	}
-	
-	public JButton getButton()
-	{
-		return button; 
-	}
-	
-	public void setText(String text)
-	{
-		label.setText(text); 
-	}
-	
-	 public void addOption(JPanel p, ButtonGroup g, String t) {
-	      JRadioButton b = new JRadioButton(t);
-	      b.setActionCommand(t);
-	      p.add(b);
-	      g.add(b);
-	   }
-		//get stuff from database 
-			//display databasae
-	
-		//get stuff from model
-			//display model
-}
