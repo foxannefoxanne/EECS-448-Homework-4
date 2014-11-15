92d2200c5edf4a2d0d6e3bf6259d1afdcb4cfa63
Christine <c316p731@ku.edu>
2014-11-15 15:08:42 -0600

    Added the classes Book and Cart, made a few changes to Model

--- src/Model.java
+++ src/Model.java
@@ -4,38 +4,67 @@ import java.io.FileReader;
 
 public class Model 
 {
-	static String[] textData; 
-		
-	public void loadData(String testFile) 
-		{
-				boolean fileNotFound = true; 
-				try
-				{
-				
-				FileReader fr = new FileReader(testFile); 
-				BufferedReader textReader = new BufferedReader(fr); 
-				
-				int numberOfLines = 60; 
-				textData = new String[numberOfLines];
-				
-				for(int i = 0; i < numberOfLines; i++)
-				{	
-					fileNotFound = false; 
-					textData[i] = textReader.readLine();
-				}
-				
-				textReader.close();
-				if(fileNotFound) {
-				throw new IOException(); 
-				} 
-				} catch (IOException e) {
-					System.out.println("File not found."); 
-				}
-		}
-		
-	public void printData() 
-	{
+//	static String[] textData; 
+//		
+//	public void loadData(String testFile) 
+//		{
+//				boolean fileNotFound = true; 
+//				try
+//				{
+//				
+//				FileReader fr = new FileReader(testFile); 
+//				BufferedReader textReader = new BufferedReader(fr); 
+//				
+//				int numberOfLines = 60; 
+//				textData = new String[numberOfLines];
+//				
+//				for(int i = 0; i < numberOfLines; i++)
+//				{	
+//					fileNotFound = false; 
+//					textData[i] = textReader.readLine();
+//				}
+//				
+//				textReader.close();
+//				if(fileNotFound) {
+//				throw new IOException(); 
+//				} 
+//				} catch (IOException e) {
+//					System.out.println("File not found."); 
+//				}
+//		}
+//		
+//	public void printData() 
+//	{
+//	
+//	}
 	
+	private void inventory(list<Book> inventoryOfBooks){
+		
 	}
 	
+	private void cart(Cart theCart){
+		
+	}
+	
+	public void addToCart(Book book){
+		
+	}
+	
+	public void removeFromCart(Book book){
+		
+	}
+	
+	private void computeTotalPrice(){
+		
+	}
+	
+	public void addToInventory(Book book){
+		
+	}
+	
+	public void removeFromInventory(Book book){
+		
+	}
+	
+	public list<Book> getBooks(Category category)
 }
