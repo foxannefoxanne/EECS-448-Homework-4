e426eff717e2ae47f0cf178b2dd1c7366dbe0c46
lynnel <lynne.lammers@gmail.com>
2014-11-15 23:43:54 -0600

    Fixing the control call in main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 
 
+
 public class Main {
 	public static void main (String[] args) 
 	{
 		Model bookStoreModel = new Model();
 		// TODO: read in each category from text file to Model's categories
+		
 		View bookStoreView = new View();
+		
         Controller bookStoreController = new Controller(bookStoreModel, bookStoreView);
-        Controller.control();
+        bookStoreController.control();
 	}
 	
 }
