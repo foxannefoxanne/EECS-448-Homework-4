f6b9df0a6fc996f69c1137d22a55ab569bff1d5c
lynnel <lynne.lammers@gmail.com>
2014-11-15 18:41:01 -0600

    Updating main to reflect new structure

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 
 
 public class Main {
-	public static void main (String[] args) {
-			
-        Controller c = new Controller();
-        c.createBookstore(); 
+	public static void main (String[] args) 
+	{
+		Model bookStoreModel = new Model();
+		// read in each category to Model's categories
+		View bookStoreView = new View();
+        Controller bookStoreController = new Controller();
+        Controller.control();
 	}
 	
 }
